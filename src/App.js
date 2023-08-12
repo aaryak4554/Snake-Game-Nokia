24a8b02aeb03e2cf0bc9be52e95b718651f92df0
aaryak4554 <joinaarya30@gmail.com>
2023-08-13 00:52:21 +0530

    added grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import "./App.css"
 
 function App() {
+
+    let gridSize = 25;
+    function renderGrid(){
+        let cellArray = [];
+        for(let row=0;row<gridSize;row++){
+            for(let col=0;col<gridSize;col++){
+                let className = "grid-item";
+                let cell = <div className={className} key={'${row}-${col}'}></div>
+                cellArray.push(cell);
+            }
+        }
+        return cellArray;
+    }
   return (
-    <div className="App">
-      yooooooooooooo
+    <div className="grid-container">
+        <h1>snake game</h1>
+        <button>start game</button>
+        <div>score:<span>30</span></div>
+        <div className="grid">{renderGrid()}</div>
     </div>
   );
 }
