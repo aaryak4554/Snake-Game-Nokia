b1897c88141b4ed9e516de4d82a6a0601be0e233
aaryak4554 <joinaarya30@gmail.com>
2023-08-25 00:49:11 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -16,14 +16,17 @@ function App() {
     const canvasRef = useRef(null);
     const [snake, setSnake] = useState(SNAKE_START);
     const [food, setFood] = useState(FOOD_START);
-    const [dir, setDir] = useState([0,1]);
-    const [speed, setSpeed] = useState(500);
+    const [dir, setDir] = useState([0,-1]);
+    const [speed, setSpeed] = useState(null);
     const [gameOver, setGameOver] = useState(false);
+
+
+    useInterval(() => gameLoop(), speed);
 
     const startGame = () => {
         setSnake(SNAKE_START);
         setFood(FOOD_START);
-        setDir([0,1]);
+        setDir([0,-1]);
         setSpeed(SPEED);
         setGameOver(false);
     }
@@ -37,7 +40,7 @@ function App() {
         keyCode>=37 && keyCode<=40 && setDir(DIRECTIONS[keyCode]);
     }
     const createFood = () => {
-
+        food.map((_a,i) => Math.floor(Math.random()*(CANVAS_SIZE[i]/SCALE)));
     }
 
     const checkCollision = (head, snk = snake) => {
@@ -48,19 +51,32 @@ function App() {
             head[1]*SCALE >= CANVAS_SIZE[1] || 
             head[1] < 0
         ) return true;
+
+        //snake tail collision
+        for(const it of snk){
+            if(head[0]===it[0] && head[1]===it[1]) return true;
+        }
         return false;
-    }
+    }   
 
-    const eatFood = () => {
-
+    const eatFood = (newSnake) => {
+        if(newSnake[0][0]===food[0] && newSnake[0][1]===food[1]){
+            let newFood = createFood();
+            while(checkCollision(newFood,newSnake)){
+                newFood = createFood();
+            }
+            setFood(newFood);
+            return true;
+        }
+        return false;
     }
 
     const gameLoop = () => {
         const snakeCopy = JSON.parse(JSON.stringify(snake));
         const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
         snakeCopy.unshift(newSnakeHead);
-        if(checkCollision(newSnakeHead)===true) endGame();
-        snakeCopy.pop();
+        if(checkCollision(newSnakeHead)) endGame();
+        if(!eatFood(snakeCopy)) snakeCopy.pop();
         setSnake(snakeCopy);
     }
 
@@ -74,7 +90,6 @@ function App() {
         context.fillRect(food[0],food[1],1,1);
     }, [snake,food,gameOver]);
 
-    useInterval(() => gameLoop(), speed);
     return (
         <div 
             className="main"
