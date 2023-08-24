7c3480ab66ef44d82636c15c02d1757f1b1c9eee
aaryak4554 <joinaarya30@gmail.com>
2023-08-24 09:43:17 +0530

    added useInterval custom hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import "./App.css";
 
 function App() {
-
-    let gridSize = 25;
-    function renderGrid(){
-        let cellArray = [];
-        for(let row=0;row<gridSize;row++){
-            for(let col=0;col<gridSize;col++){
-                let className = "grid-item";
-                let cell = <div className={className} key={`${row}-${col}`}></div>
-                cellArray.push(cell);
-            }
-        }
-        return cellArray;
-    }
-  return (
-    <div className="grid-container">
-        <h1>snake game</h1>
-        <button>start game</button>
-        <div>score:<span>30</span></div>
-        <div className="grid">{renderGrid()}</div>
-    </div>
-  );
+    return (
+        <div>
+            <div className='main-container'>
+                <div className="score-details">
+                    <span>Score: </span>
+                    <span> Highest Score: </span>
+                </div>
+                <div className="snake-grid"></div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,17 @@
+const SNAKE_START=[
+    [2,3],
+    [2,4]
+];
+
+const FOOD_START=[6,7];
+
+const SPEED=100;
+
+const SCALE=50;
+
+export{
+    SNAKE_START,
+    FOOD_START,
+    SPEED,
+    SCALE
+};
